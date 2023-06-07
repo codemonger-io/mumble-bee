8f6df1cc13de958189b0aba4399043020eee332a
Kikuo Emoto <kemoto@codemonger.io>
2023-06-07 12:05:40 +0900

    fix(app): add global
    - Fixes the issue that Amplify did not work due to missing `global`.
      Lets Vite resolve `global` to `window`.
    
      Reference: https://github.com/bevacqua/dragula/issues/602#issuecomment-1296313369

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,6 +6,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  define: {
+    global: 'window', // resolves: global is not defined. see https://github.com/bevacqua/dragula/issues/602#issuecomment-1296313369
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
