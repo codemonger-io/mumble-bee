1172a44ee3094170355575221c44fe6e3d67f281
Kikuo Emoto <kemoto@codemonger.io>
2023-06-07 12:43:41 +0900

    feat(app): add current user store
    - Introduces a Pinia store for the current user in
      `src/stores/current-user.ts`.
    
      It stores the current user and the configuration of the user. It
      fetches the configuration of the user as soon as the current user is
      replaced.
    
      It depends on `MumbleApi`, and it must be provided via "mumbleApi".
      The app is supposed to `provide` it.

---
+++ app/src/stores/current-user.ts
@@ -0,0 +1,42 @@
+import type { CognitoUser } from 'amazon-cognito-identity-js'
+import { defineStore } from 'pinia'
+import { inject, ref, watch } from 'vue'
+
+import type { MumbleApi } from '@/lib/mumble-api'
+import type { UserConfig } from '@/types/user-config'
+
+/**
+ * Store for the current user.
+ *
+ * @remarks
+ *
+ * Stores also the configuration for the user.
+ *
+ * You have to `provide` a {@link MumbleApi} via "mumbleApi" before using this
+ * store.
+ *
+ * @beta
+ */
+export const useCurrentUser = defineStore('current-user', () => {
+  const mumbleApi: MumbleApi = inject('mumbleApi')
+
+  const user = ref<CognitoUser | undefined>()
+
+  const userConfig = ref<UserConfig | undefined>()
+  watch(user, async user => {
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('[useCurrentUser]', 'updating user', user)
+    }
+    if (user != null) {
+      try {
+        userConfig.value = await mumbleApi.getUserConfig(user)
+      } catch (err) {
+        console.error('[useCurrentUser]', 'failed to get user config', err)
+      }
+    } else {
+      userConfig.value = undefined
+    }
+  })
+
+  return { user, userConfig }
+})
