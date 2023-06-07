19f5fcca8c31901e0bbee37c92d3742f0e1c8ec8
Kikuo Emoto <kemoto@codemonger.io>
2023-06-07 12:50:31 +0900

    feat(app): add authenticator with Hosted UI
    - Introduces a new component `HostedUiAuthenticator` that guards
      contents with authentication. It shows a "Sign In" link if no user is
      authenticated, otherwise, shows the child contents.
    
      It navigates the user to the Cognito hosted UI, if the user clicks the
      "Sign In" link.
    
      It supposes that the app or a parent component `provide` a `MumbleApi`
      via "mumbleApi", as it depends on the "current-user" store.

---
+++ app/src/components/HostedUiAuthenticator.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { useAuthenticator } from '@aws-amplify/ui-vue'
+import { Auth } from 'aws-amplify'
+import { computed, ref, watchEffect } from 'vue'
+
+import { useCurrentUser } from '@/stores/current-user'
+
+const authenticator = useAuthenticator()
+
+// the app or a parent component must `provide` "mumbleApi"
+const currentUser = useCurrentUser()
+watchEffect(() => currentUser.user = authenticator.user)
+
+const signIn = () => {
+  Auth.federatedSignIn()
+}
+</script>
+
+<template>
+  <div v-if="authenticator.authStatus === 'authenticated'">
+    <slot></slot>
+  </div>
+  <div v-else-if="authenticator.authStatus === 'configuring'">
+    <p>Checking the session...</p>
+  </div>
+  <div v-else>
+    <a @click="signIn()">Sign In</a>
+  </div>
+</template>
