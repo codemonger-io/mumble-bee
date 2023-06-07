8b1da15a9b4c869e67af2f33fb909239788fbf6a
Kikuo Emoto <kemoto@codemonger.io>
2023-06-07 12:58:19 +0900

    feat(app): configure Amplify and Mumble API
    - `src/main.ts` configures Amplify and Mumble API.
    
      It `provide`s the Mumble API client through "mumbleApi" so that the
      "current-user" store can access the Mumble API.

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,14 +1,25 @@
 import './assets/main.css'
 
+import { Amplify } from 'aws-amplify'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+import authConfig from '@/configs/auth-config'
+import { mumbleApi } from '@/lib/mumble-api'
+
+Amplify.configure({
+  Auth: authConfig,
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.provide('mumbleApi', mumbleApi)
 
 app.mount('#app')
