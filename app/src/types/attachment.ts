bb3988c88a284723eec51f95a602419b18b2d41a
Kikuo Emoto <kemoto@codemonger.io>
2023-06-08 18:59:55 +0900

    feat(app): add Attachment
    - Introduces a new type `Attachment` that represents an attachment and
      its uploading/deleting state.

---
+++ app/src/types/attachment.ts
@@ -0,0 +1,48 @@
+/**
+ * Attachment with the state.
+ *
+ * @beta
+ */
+export interface Attachment {
+  /** ID of the attachment. */
+  readonly id: string
+
+  /** Name of the file. */
+  readonly filename: string
+
+  /** State of the attachment. */
+  state: AttachmentState
+}
+
+/**
+ * State of an attachment.
+ *
+ * @beta
+ */
+export type AttachmentState = 'uploading' | 'uploaded' | 'deleting'
+
+/**
+ * Creates an {@link Attachment} from a given `File`.
+ *
+ * @remarks
+ *
+ * The ID of the attachment consists of two parts:
+ * - a unique part generated randomly (UUID v4)
+ * - the extension of the file (empty if the file has no extension)
+ *
+ * @beta
+ */
+export function createAttachment(file: File): Attachment {
+  const uniquePart = crypto.randomUUID().toString('hex')
+  const extIndex = file.name.lastIndexOf('.')
+  const ext = extIndex !== -1 ? file.name.substring(extIndex) : ''
+  const id = uniquePart + ext
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('[createAttachment]', 'generated ID', id)
+  }
+  return {
+    id,
+    filename: file.name,
+    state: 'uploading',
+  }
+}
