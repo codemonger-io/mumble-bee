26fffe1977dcb6018256fdb0563099f1dbebc639
Kikuo Emoto <kemoto@codemonger.io>
2023-06-08 19:15:18 +0900

    feat(app): add PostEditor
    - Introduces a new component `PostEditor` that provides an interface to
      edit a new post. `PostEditor` includes another component
      `PostEditorControls` that provides interface to
        - attach/detach files
        - submit the post

---
+++ app/src/components/PostEditor.vue
@@ -0,0 +1,169 @@
+<script setup lang="ts">
+import { computed, reactive, ref, watch } from 'vue'
+
+import { Auth } from 'aws-amplify'
+import {
+  DeleteObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3'
+import type { Credentials, Provider } from '@aws-sdk/types'
+
+import authConfig from '@/configs/auth-config'
+import { createCredentialsProvider } from '@/lib/credentials-provider'
+import { useCurrentUser } from '@/stores/current-user'
+import type { Attachment } from '@/types/attachment'
+import { createAttachment } from '@/types/attachment'
+
+import PostEditorControls from './PostEditorControls.vue'
+
+const currentUser = useCurrentUser()
+const credentialsProvider = computed<Provider<Credentials | undefined>>(() => {
+  const user = currentUser.user
+  if (user != null) {
+    return createCredentialsProvider(user)
+  } else {
+    return undefined
+  }
+})
+
+const content = ref<string>('')
+const attachments = reactive([] as Attachment[])
+
+const onSubmit = () => {
+  console.log('[PostEditor]', 'submitting', content.value)
+}
+
+const uploadAttachment = async (
+  file: File,
+  bucketName: string,
+  credentials: Provider<Credentials>,
+) => {
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('[PostEditor]', 'uploading attachment', file)
+  }
+  attachments.push(createAttachment(file))
+  const id = attachments[attachments.length - 1].id
+  const username = currentUser.user!.getUsername()
+  const client = new S3Client({
+    region: authConfig.region,
+    credentials,
+  })
+  try {
+    const res = await client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: `media/users/${username}/${id}`,
+        Body: file,
+        ContentType: file.type,
+      }),
+    )
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('[PostEditor]', 'finished uploading attachment', file, res)
+    }
+    attachments.find(a => a.id === id)!.state = 'uploaded'
+  } catch (err) {
+    if (err.toString().startsWith('NotAuthorizedException:')) {
+      if (process.env.NODE_ENV !== 'production') {
+        console.log('[PostEditor]', 'refreshing credentials')
+      }
+      Auth.currentAuthenticatedUser()
+        .catch(err => {
+          console.error('[PostEditor]', 'failed to refresh credentials', err)
+        })
+    } else {
+      console.error('[PostEditor]', 'failed to upload attachment', err)
+    }
+    const index = attachments.findIndex(a => a.id === id)
+    if (index !== -1) {
+      attachments.splice(index, 1)
+    }
+  }
+}
+
+const deleteAttachment = async (
+  id: string,
+  bucketName: string,
+  credentials: Provider<Credentials>,
+) => {
+  const attachment = attachments.find(a => a.id === id)
+  if (attachment == null) {
+    return
+  }
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('[PostEditor]', 'deleting attachment', attachment)
+  }
+  const username = currentUser.user!.getUsername()
+  attachment.state = 'deleting'
+  const client = new S3Client({
+    region: authConfig.region,
+    credentials,
+  })
+  const res = await client.send(
+    new DeleteObjectCommand({
+      Bucket: bucketName,
+      Key: `media/users/${username}/${id}`,
+    }),
+  )
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('[PostEditor]', 'finished deleting attachment', attachment, res)
+  }
+  const index = attachments.indexOf(attachment)
+  if (index !== -1) {
+    attachments.splice(index, 1)
+  }
+}
+
+const attachmentQueue = reactive([])
+watch(
+  [
+    attachmentQueue,
+    () => currentUser.userConfig?.objectsBucketName,
+    credentialsProvider,
+  ],
+  ([queue, bucketName, credentials]) => {
+    if (queue.length > 0 && bucketName != null && credentials != null) {
+      const file = queue.pop()
+      uploadAttachment(file, bucketName, credentials)
+    }
+  },
+)
+
+const onAttachmentAdded = (file: File) => {
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('[PostEditor]', 'attachment added', file)
+  }
+  attachmentQueue.push(file)
+}
+
+const onAttachmentDeleted = (attachment: Attachment) => {
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('[PostEditor]', 'attachment deleted', attachment)
+  }
+  const bucketName = currentUser.userConfig?.objectsBucketName
+  const credentials = credentialsProvider.value
+  if (bucketName == null || credentials == null) {
+    return
+  }
+  deleteAttachment(attachment.id, bucketName, credentials)
+}
+</script>
+
+<template>
+  <form @submit.prevent="onSubmit">
+    <b-field label="What are you going to mumble?">
+      <b-input
+        type="textarea"
+        v-model="content"
+        placeholder="What are you going to mumble?"
+        @keyup.enter.shift="onSubmit"
+      >
+      </b-input>
+    </b-field>
+    <PostEditorControls
+      :attachments="attachments"
+      @attachment-added="onAttachmentAdded"
+      @attachment-deleted="onAttachmentDeleted"
+    />
+  </form>
+</template>
---
+++ app/src/components/PostEditorControls.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+
+import type { Attachment } from '@/types/attachment'
+
+const props = defineProps<{
+  attachments: Attachment[],
+}>()
+const emit = defineEmits<{
+  (e: 'attachment-added', file: File): void
+  (e: 'attachment-deleted', attachment: Attachment): void
+}>()
+
+const file = ref<File | undefined>()
+watch(file, file => {
+  if (file != null) {
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('[PostEditorControls]', 'selected attachment', file.name)
+    }
+    emit('attachment-added', file)
+    // waits for another file
+    file.value = undefined
+  }
+})
+
+const getAttachmentIcon = (state: AttachmentState) => {
+  switch (state) {
+    case 'uploading':
+      return 'upload'
+    case 'uploaded':
+      return 'check'
+    case 'deleting':
+      return 'delete'
+    default:
+      const exhaustiveCheck: never = state
+      throw new RangeError('unknown attachment state: ' + state)
+  }
+}
+const getAttachmentType = (state: AttachmentState) => {
+  switch (state) {
+    case 'uploading':
+      return 'is-warning'
+    case 'uploaded':
+      return 'is-info'
+    case 'deleting':
+      return 'is-danger'
+    default:
+      const exhaustiveCheck: never = state
+      throw new RangeError('unknown attachment state: ' + state)
+  }
+}
+</script>
+
+<template>
+  <div class="field">
+    <div class="level">
+      <div class="level-left">
+        <b-field class="file is-info">
+          <b-upload v-model="file" class="file-label">
+            <span class="file-cta">
+              <b-icon class="file-icon is-icon-only" icon="paperclip"></b-icon>
+            </span>
+          </b-upload>
+        </b-field>
+      </div>
+      <div class="level-right">
+        <input type="submit" class="button is-primary" value="Mumble">
+      </div>
+    </div>
+  </div>
+  <div class="attachments">
+    <h3>{{ attachments.length > 0 ? 'Attachments' : 'No attachments' }}</h3>
+    <b-taglist>
+      <b-tag
+        v-for="attachment in attachments"
+        :key="attachment.id"
+        :icon="getAttachmentIcon(attachment.state)"
+        :type="getAttachmentType(attachment.state)"
+        closable
+        @close="emit('attachment-deleted', attachment)"
+      >
+        {{ attachment.filename }}
+      </b-tag>
+    </b-taglist>
+  </div>
+</template>
+
+<style scoped>
+.file-cta {
+  .file-icon {
+    &.is-icon-only {
+      margin-right: 0;
+    }
+  }
+}
+</style>
