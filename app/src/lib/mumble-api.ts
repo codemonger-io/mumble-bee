42b5c346cea2848f2687985ffe60e2568d36f265
Kikuo Emoto <kemoto@codemonger.io>
2023-06-10 01:03:29 +0900

    feat(app): add submit post
    - `MumbleApi` introduces a new method `submitPost` that submits a given
      Mumble post of a specified user. `MumbleApiImpl` implements
      `submitPost` and sends a post to the endpoint
      `/users/{username}/outbox`.

--- app/src/lib/mumble-api.ts
+++ app/src/lib/mumble-api.ts
@@ -1,6 +1,8 @@
 import type { CognitoUser } from 'amazon-cognito-identity-js'
 
 import apiConfig from '@/configs/api-config'
+import { ACTIVITY_STREAMS_MIME_TYPE } from './activity-streams'
+import type { MumblePost } from '@/types/mumble-post'
 import type { UserConfig } from '@/types/user-config'
 import { isUserConfig } from '@/types/user-config'
 
@@ -22,6 +24,9 @@ export interface MumbleApiConfig {
 export interface MumbleApi {
   /** Returns the configuration for a given user. */
   getUserConfig(user: CognitoUser): Promise<UserConfig>
+
+  /** Submits a given post from a specified user. */
+  submitPost(user: CognitoUser, post: MumblePost): Promise<void>
 }
 
 /**
@@ -50,6 +55,23 @@ export class MumbleApiImpl implements MumbleApi {
       throw new Error(`invalid user config: ${data}`)
     }
     return data
+  }
+
+  async submitPost(user: CognitoUser, post: MumblePost): Promise<void> {
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('[MumbleApiImpl]', 'submitting post:', user, post)
+    }
+    const session = user.getSignInUserSession()
+    const idToken = session.getIdToken().getJwtToken()
+    const url = `${this.config.baseUrl}/users/${user.getUsername()}/outbox`
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        Authorization: idToken,
+        'Content-Type': ACTIVITY_STREAMS_MIME_TYPE,
+      },
+      body: JSON.stringify(post),
+    })
   }
 }
 
