a941b83d28bb3f44a31983fd83e44760d2379dd4
Kikuo Emoto <kemoto@codemonger.io>
2023-06-10 00:48:40 +0900

    feat(app): add activity streams utils
    - Introduces a new utility `src/lib/activity-streams.ts` that defines
      constants related to Activity Streams.
        - MIME type
        - public address

---
+++ app/src/lib/activity-streams.ts
@@ -0,0 +1,14 @@
+/**
+ * MIME type for a payload in Activity Streams.
+ *
+ * @beta
+ */
+export const ACTIVITY_STREAMS_MIME_TYPE = 'application/activity+json'
+
+/**
+ * Public address in Activity Streams.
+ *
+ * @beta
+ */
+export const ACTIVITY_STREAMS_PUBLIC_ADDRESS =
+  'https://www.w3.org/ns/activitystreams#Public'
