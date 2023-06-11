715581fd5346d6916bdcbb1076a83ee1047bb3d6
Kikuo Emoto <kemoto@codemonger.io>
2023-06-11 16:30:57 +0900

    feat(app): add Post component
    - Introduces a new component `Post` that renders a single post.

---
+++ app/src/components/Post.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { Post } from '@/types/post'
+
+defineProps<{
+  post: Post,
+}>()
+</script>
+
+<template>
+  <div class="card block">
+    <div class="card-content">
+      <div class="content">
+        <p class="post-content">{{ post.content }}</p>
+        <p class="post-date">{{ post.published }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.post-date {
+  font-size: 0.8em;
+}
+</style>
