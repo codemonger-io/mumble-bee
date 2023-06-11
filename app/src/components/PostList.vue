9d67b2e25b2ced70d6f3b9ebd171b23a805aa48d
Kikuo Emoto <kemoto@codemonger.io>
2023-06-11 17:29:10 +0900

    feat(app): add PostList component
    - Introduces a new component `PostList` that just shows the `PostEditor`
      and `RecentPosts`.

---
+++ app/src/components/PostList.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import PostEditor from './PostEditor.vue'
+import RecentPosts from './RecentPosts.vue'
+</script>
+
+<template>
+  <div class="post-editor block">
+    <PostEditor />
+  </div>
+  <div class="recent-posts block">
+    <RecentPosts />
+  </div>
+</template>
