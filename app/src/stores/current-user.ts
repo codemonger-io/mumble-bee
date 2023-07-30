b6308b2a845a1e553b45ea112e161ff908393291
Kikuo Emoto <kemoto@codemonger.io>
2023-07-30 15:52:40 +0900

    feat(app): add refreshSession
    - `current-user` store introduces a new action `refreshSession` that
      refreshes the ID token of the current user. It resets the `user` state
      to `null` if it fails to refresh the session.
    
    issue codemonger-io/mumble-bee#4

--- app/src/stores/current-user.ts
+++ app/src/stores/current-user.ts
@@ -20,9 +20,28 @@ import type { UserConfig } from '@/types/user-config'
 export const useCurrentUser = defineStore('current-user', () => {
   const mumbleApi: MumbleApi = inject('mumbleApi')!
 
-  const user = shallowRef<CognitoUser | undefined>()
+  const user = shallowRef<CognitoUser | null>(null)
 
-  const userConfig = ref<UserConfig | undefined>()
+  const refreshSession = async () => {
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('[useCurrentUser]', 'refreshing session')
+    }
+    const currentUser = user.value
+    if (currentUser != null) {
+      user.value = await new Promise(resolve => {
+        currentUser.getSession((err: any) => {
+          if (err != null) {
+            console.error('[useCurrentUser]', 'failed to refresh session', err)
+            resolve(null)
+          }
+          resolve(currentUser)
+        })
+      })
+    }
+    return user.value
+  }
+
+  const userConfig = ref<UserConfig | null>(null)
   watch(user, async user => {
     if (process.env.NODE_ENV !== 'production') {
       console.log('[useCurrentUser]', 'updating user', user)
@@ -34,9 +53,9 @@ export const useCurrentUser = defineStore('current-user', () => {
         console.error('[useCurrentUser]', 'failed to get user config', err)
       }
     } else {
-      userConfig.value = undefined
+      userConfig.value = null
     }
   })
 
-  return { user, userConfig }
+  return { user, userConfig, refreshSession }
 })
