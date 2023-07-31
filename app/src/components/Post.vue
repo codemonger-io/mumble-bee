dc6bf6a24f60470cffda4d57e514e9a226468ef5
Kikuo Emoto <kemoto@codemonger.io>
2023-07-31 16:27:13 +0900

    feat(app): embed HTML post
    - `Post` embeds HTML posts on the page. Posts are processed by
      `DOMPurify` before being embedded
    
    issue codemonger-io/mumble-bee#1

--- app/src/components/Post.vue
+++ app/src/components/Post.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
+import DOMPurify from 'dompurify'
+import { computed } from 'vue'
+
 import type { Post } from '@/types/post'
 
-defineProps<{
+const props = defineProps<{
   post: Post,
 }>()
+
+const safeContent = computed(() => {
+  return DOMPurify.sanitize(props.post.content)
+})
 </script>
 
 <template>
   <div class="card block">
     <div class="card-content">
       <div class="content">
-        <p class="post-content">{{ post.content }}</p>
+        <div class="post-content markdown-content" v-html="safeContent"></div>
         <p class="post-date">{{ post.published }}</p>
       </div>
     </div>
